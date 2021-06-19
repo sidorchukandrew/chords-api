334ad59b879fe07c0a2781871b690fac5d2396c8
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-06-18 23:04:59 -0400

    Configured production for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+    config.action_mailer.default_url_options = { :host => ENV["API_HOSTNAME"] }
+
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["EMAIL"],
+    :password             => ENV["EMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
