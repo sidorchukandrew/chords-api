956e6bfaf547a5b1302cde71cac42be3653f938e
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-09-07 20:50:04 -0400

    updated the invitation email format

--- test/mailers/previews/invitation_mailer_preview.rb
+++ test/mailers/previews/invitation_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
 
+    def invite_member
+        invitation = Invitation.new(token: "ergj0394ut9344g845g094gb", email: "someemail@cadencechords.com")
+
+        sender = User.find_by(is_admin: true)
+
+        InvitationMailer.with(invitation: invitation, sender: sender).invite_member
+    end
+
 end
