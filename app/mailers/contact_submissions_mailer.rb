248101a9ecebc5367bb8fd1897818ddcae59319d
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-10-25 00:37:20 -0400

    Added logging

--- app/mailers/contact_submissions_mailer.rb
+++ app/mailers/contact_submissions_mailer.rb
@@ -2,6 +2,9 @@ class ContactSubmissionsMailer < ApplicationMailer
 
   def contact_submitted
     @submission = params[:submission]
+    puts '------------------------------------------'
+    puts @submission
+    puts '------------------------------------------'
     mail(to: ENV['EMAIL'], subject: 'Contact form submitted')
   end
 end
