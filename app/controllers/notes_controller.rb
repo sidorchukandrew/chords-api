870444c46d42a2b83b3c4089fab3f605359d18b8
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-04-12 20:28:59 -0400

    Removed content

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -65,8 +65,7 @@ class NotesController < ApplicationController
         "     color: #{@note.color},\n" +
         "     song:  #{@note.song.name},\n" +
         "     x: #{@note.x},\n" +
-        "     y: #{@note.y},\n" +
-        "     content: #{@note.content}\n"
+        "     y: #{@note.y}\n"
         "}"
       Telegram.send_message(message)
     end
