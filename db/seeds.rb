41d22c9b63bda795573a0a2bcaed33424608b73e
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-07-13 23:52:27 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -182,5 +182,5 @@ end
 teams = Team.all
 
 teams.each do |team|
-    team.join_link = SecureRandom.hex(10) unless team.join_link
+    team.join_link = SecureRandom.hex(10) unless team.join_link != ''
 end
